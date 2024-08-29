9e3f823af2a868b3a97110acee25edf3fe8dd7f4
Ajay <ajaybirh@gmail.com>
2024-08-28 20:29:12 -0700

    hi

---
+++ Desktop/Project_Blue/BlueBlue/main.lua
@@ -0,0 +1,22 @@
+-----------------------------------------------------------------------------------------
+--
+-- main.lua
+--
+-----------------------------------------------------------------------------------------
+
+-- hide the status bar
+display.setStatusBar( display.HiddenStatusBar )
+print("Kachirro")
+
+-- include the Corona "composer" module
+local composer = require "composer"
+
+-- load menu screen
+composer.gotoScene( "menu" )
+
+
+
+
+
+
+
